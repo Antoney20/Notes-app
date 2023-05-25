3e21c9b39d1c7ec4d2df42910aa40d168554a628
Muchiri Antony Mutwiri <101004140+Antoney20@users.noreply.github.com>
2023-05-25 16:10:23 +0300

    The main Activity upload

---
+++ MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addNote;
+    ImageButton logoutBtn;
+    RecyclerView recyclerView;
+    NoteAdapter noteAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addNote = findViewById(R.id.add_notes);
+        logoutBtn = findViewById(R.id.logout);
+        recyclerView = findViewById(R.id.recyclerview);
+        setUpRecyclerView();
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NotesDetails.class));
+            }
+        });
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NotesDetails.class));
+            }
+        });
+
+
+    }
+
+    private void setUpRecyclerView() {
+       Query query = Utility.getCollectionRefferenceNotes().orderBy("timestamp", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options, this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+    }
+}
